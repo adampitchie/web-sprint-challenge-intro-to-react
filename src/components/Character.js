f807faaad7aa7d6f0ec9b25e3a802fd6751bfbf0
Adam Pitchie <adampitchie@gmail.com>
2021-03-19 11:52:52 -0700

    finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Character(props) {
+  const {character} = props
+
+  const StyledH3 = styled.h3`
+    color: white
+  `
+
+  return (
+    <StyledH3>{character.name}</StyledH3>
+  )
+}
